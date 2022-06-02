7cb0eb89fb2c43808650cd3eb69fdb75573ceea1
E-Trinh <ethantrinh@csus.edu>
2022-06-01 22:00:40 -0700

    Added navigation bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import './styles.css';
 import { homePage } from './home.js'
 
+function navBar() {
+    const div = document.createElement("div");
+    div.classList.toggle("nav-bar");
+
+    const home = document.createElement("button");
+    home.classList.toggle("nav-button");
+    home.textContent = "Home";
+
+    const menu = document.createElement("button");
+    home.classList.toggle("nav-button");
+    menu.textContent = "Menu";
+
+    const contact = document.createElement("button");
+    home.classList.toggle("nav-button");
+    contact.textContent = "Contact";
+
+    div.appendChild(home);
+    div.appendChild(menu);
+    div.appendChild(contact);
+    
+    return div;
+}
+
+document.getElementById("content").appendChild(navBar());
 document.getElementById("content").appendChild(homePage());
